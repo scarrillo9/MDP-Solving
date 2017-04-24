ed62b4401cef153d965267c4a27f6b56fa435620
Stefany Carrillo <scarrillo7@miners.utep.edu>
2017-04-23 23:52:13 -0600

    value iteration working/done

--- main.java
+++ main.java
@@ -1,8 +1,18 @@
+/* Hector Cervantes and Stefany Carrillo
+ * Artificial Intelligence: Markov Decision Processes
+ * Monte Carlo, Value Iteration, Q Learning
+ */
 import java.text.DecimalFormat;
 import java.util.concurrent.ThreadLocalRandom;
 import java.util.Scanner;
+import java.util.HashMap;
+import java.util.Hashtable;
+import java.util.Random;
 
 public class main {
+	static Random r = new Random();
+	static HashMap<String, Integer> iterationCount = new HashMap<String, Integer>();
+	static HashMap<String, Double> currentValue = new HashMap<String, Double>();
 	static DecimalFormat df = new DecimalFormat("#.##");
 	static State end = new State();
 	static State TU10a = new State("T", "U", "10AM", null, 0, null, 0, null, 0, end, -1, 1);
@@ -18,6 +28,7 @@ public class main {
 	static State start = RU8p;
 	
 	public static void main(String[] args){
+		State rstart = initStates();
 		Scanner input = new Scanner(System.in);
 		System.out.println("Choose Markov Decision Process:\nMonte Carlo? (m)"
 				+ "\nValue Iteration? (i)\nQ Learning? (q)");
@@ -25,11 +36,47 @@ public class main {
 		
 		if(option.equals("m"))
 			monteCarlo(start);
-		//if(option.equals("i"))
-			
+		if(option.equals("i"))
+			valueIterationInit(rstart);
 		if(option.equals("q"))	
 			qLearning(start);
 	}//end main method
+	
+	public static State initStates(){ //party // rest // study
+		State end = new State();
+		State TU10a = new State("tired", "undone", "10AM", null, 0, null, 0, null, 0, end, -1, 1);
+		currentValue.put(TU10a.health + " " + TU10a.homework+ " " + TU10a.time, 1.0);
+		iterationCount.put(TU10a.health + " " + TU10a.homework + " " + TU10a.time, 0);
+		State RU10a = new State("rested", "undone", "10AM", null, 0, null, 0, null, 0, end, 0, 1);
+		currentValue.put(RU10a.health + " " + RU10a.homework + " "+ RU10a.time, 0.0);
+		iterationCount.put(RU10a.health + " "+ RU10a.homework + " " + RU10a.time, 0);
+		State RD10a = new State("rested", "done", "10AM", null, 0, null, 0, null, 0, end, 4, 1);
+		currentValue.put(RD10a.health + " " + RD10a.homework +" "+ RD10a.time, 0.0);
+		iterationCount.put(RD10a.health +" "+ RD10a.homework +" "+ RD10a.time, 0);
+		State TD10a = new State("tired", "done", "10AM", null, 0, null, 0, null, 0, end, 3, 1);
+		currentValue.put(TD10a.health + " "+ TD10a.homework + " " + TD10a.time, 0.0);
+		iterationCount.put(TD10a.health + " " + TD10a.homework + " " + TD10a.time, 0);
+		State RU8a = new State("rested", "undone", "8AM", TU10a, 2, RU10a, 0, RD10a, -1, null, 0, 3);
+		currentValue.put(RU8a.health + " "+ RU8a.homework +" "+ RU8a.time, 0.0);
+		iterationCount.put(RU8a.health + " " + RU8a.homework +" "+ RU8a.time, 0);
+		State RD8a = new State("rested", "done", "8AM", TD10a, 2, RD10a, 0, null, 0, null, 0, 2);
+		currentValue.put(RD8a.health + " " + RD8a.homework + " " + RD8a.time, 0.0);
+		iterationCount.put(RD8a.health + " " + RD8a.homework +" " + RD8a.time, 0);
+		State TU10p = new State("tired", "undone", "10PM", RU10a, 2, RU8a, 0, null, 0, null, 0, 2);
+		currentValue.put(TU10p.health + " " + TU10p.homework + " " + TU10p.time, 0.0);
+		iterationCount.put(TU10p.health + " " + TU10p.homework +" "+ TU10p.time, 0);
+		State RU10p = new State("rested", "undone", "10PM", RU8a, 2, RU8a, 0, RD8a, -1, null, 0, .5, RU10a, 2, 3);
+		currentValue.put(RU10p.health + " " +RU10p.homework + " "+  RU10p.time, 0.0);
+		iterationCount.put(RU10p.health +" "+ RU10p.homework +" "+ RU10p.time, 0);
+		State RD10p = new State("rested", "done", "10PM", RU8a, 2, RD8a, 0, null, 0, null, 0, .5, RD10a, 2, 2);
+		currentValue.put(RD10p.health +" "+ RD10p.homework + " "+ RD10p.time, 0.0);
+		iterationCount.put(RD10p.health +" "+ RD10p.homework +" "+ RD10p.time, 0);
+		State RU8p = new State("rested", "undone", "8PM", TU10p, 2, RU10p, 0, RD10p, -1, null, 0, 3);
+		currentValue.put(RU8p.health + " " + RU8p.homework +" "+ RU8p.time, 0.0);
+		iterationCount.put(RU8p.health + " " + RU8p.homework +" "+ RU8p.time, 0);
+		
+		return RU8p;
+	}//end initStates
 	
 	public static void qLearning(State start){
 		qArrays(); //setting up arrays for Q values
@@ -70,6 +117,35 @@ public class main {
 		stateValuesQ();
 	}//end qLearning
 	
+	private static void valueIterationInit(State start) {
+		int i = 0;
+		int iterations = 100;
+	    while(i < iterations){
+			valueIteration(start);
+			i++;
+		}
+	    System.out.println();
+	    for (String name: iterationCount.keySet()){
+
+            String key = name.toString();
+            String value = iterationCount.get(name).toString();  
+            System.out.println("Number of iterations for " + key + " " + value);  
+	    }
+	    System.out.println();
+	    for (String name: currentValue.keySet()){
+
+            String key = name.toString();
+            String value = currentValue.get(name).toString();  
+            System.out.println("Current values for " + key + " " + value);  
+	    }
+		
+	    System.out.println("");
+	    System.out.println("Optimality");
+	    printOptimal(start);
+	    
+	    
+	}
+	
 	public static void monteCarlo(State start){
 		double alpha = 0.1;
 		State temp = start;
@@ -101,6 +177,108 @@ public class main {
 		System.out.println("\n\nAverage reward for Monte Carlo: " + avgReward);
 	}//end monteCarlo 
 	
+	private static void printOptimal(State start) {
+		if(start.health == null){
+			return;
+		}
+		String state = start.health+ " " + start.homework + " " + start.time;
+		System.out.println(state + " value: " + currentValue.get(state));
+		State optimalState = start;
+		String maxNeighbor = " ";
+		double max = 0.0;
+		String neighbor;
+		if(start.afterParty != null){
+			neighbor = start.afterParty.health + " " + start.afterParty.homework + " " + start.afterParty.time;
+			if(currentValue.get(neighbor) >= max){
+				max = currentValue.get(neighbor);
+				optimalState = start.afterParty;
+				maxNeighbor = neighbor;
+			}
+		}
+		if(start.afterRest != null){
+			neighbor = start.afterRest.health + " " + start.afterRest.homework + " " + start.afterRest.time;
+			if(currentValue.get(neighbor) >= max){
+				max = currentValue.get(neighbor);
+				optimalState = start.afterRest;
+				maxNeighbor = neighbor;
+			}
+		}
+		if(start.afterStudy != null){
+			neighbor = start.afterStudy.health + " " + start.afterStudy.homework + " " + start.afterStudy.time;
+			if(currentValue.get(neighbor) >= max){
+				max = currentValue.get(neighbor);
+				optimalState = start.afterStudy;
+				maxNeighbor = neighbor;
+			}
+		}
+		if(start.afterAny != null){
+			neighbor = start.afterAny.health + " " + start.afterAny.homework + " " + start.afterAny.time;
+			if(currentValue.get(state) >= max){
+				max = currentValue.get(state);
+				optimalState = start.afterAny;
+				maxNeighbor = neighbor;
+			}
+		}
+		
+		printOptimal(optimalState);
+		
+	}
+	
+	private static boolean valueIteration(State start) {
+		if(start.health == null){
+			return true;
+		}
+		String state = start.health +" "+ start.homework +" "+ start.time;
+		System.out.printf("%s/%s Time: %s\n", start.health, start.homework, start.time);
+		System.out.println("Current Value: " + currentValue.get(state));
+		State neighbor = start;
+		int action = r.nextInt(start.actions - 1 + 1) + 1;
+		double value = 0.0;
+		
+		if(start.afterAny != null){
+			neighbor = start.afterAny;
+			value = (1.0/3.0) * (start.anyReward + .99 * 1.0);
+		}
+		else{
+			if(action == 1 && start.transitionState){
+				int tState = r.nextInt(2 - 1 + 1) + 1;
+				if(tState == 1){
+					neighbor = start.afterParty;
+					String neighborName = neighbor.health +" " + neighbor.homework + " " + neighbor.time;
+					value = .5 * (getReward(start, 1) + .99 * currentValue.get(neighborName));
+				}
+
+				else{
+					neighbor = start.afterParty2;
+					String neighborName = neighbor.health + " " + neighbor.homework + " "+ neighbor.time;
+					value = .5 * (getReward(start, 1) + .99 * currentValue.get(neighborName));
+				}
+			}
+			else{
+				neighbor = getState(start, action);
+				String neighborName = neighbor.health + " "+ neighbor.homework + " " + neighbor.time;
+				value = (1/ (double) start.actions) * (getReward(start, 1) + .99 * currentValue.get(neighborName));
+			}
+		}
+		System.out.println("Next Value: " + value);
+		System.out.println("Action Taken: " + getAction(action));
+		// count the number of times state changes
+		iterationCount.put(state, iterationCount.get(state) + 1);
+		// update values
+		currentValue.put(state, value);
+		
+		return valueIteration(neighbor);
+	}
+	
+	private static String getAction(int actionTaken) {
+		switch(actionTaken){
+		case 1: return "Party";
+		case 2: return "Rest";
+		case 3: return "Study";
+		case 5: return "Party";
+		default: return "Any";
+		}
+	}
 	public static void stateValues(){
 		System.out.printf("Values of States:\n");
 		System.out.printf("State: RU8p, Value: " + RU8p.value);
